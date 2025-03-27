d91a0992c4f4ee68a15c4154b4fd15bca1ec0f59
Prakhar <rampalliprakhar@gmail.com>
2025-03-26 23:05:28 -0400

    Initial Testing: Link

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,12 @@
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = process.env.MAIN_URL;
-
 const URL = process.env.NODE_ENV === 'production' 
-    ? BACKEND_URL
+    ? 'https://whiteboardserver-drir.onrender.com'
     : 'http://localhost:5000';
 
 export const socket = io(URL, {
     transports: ['websocket'],
     cors:{
-        origin: BACKEND_URL
+        origin: 'https://whiteboardserver-drir.onrender.com'
     }
 });
