e4571f5437fdbe6383af377a3aafe6a2a185a277
Prakhar <rampalliprakhar@gmail.com>
2025-03-26 22:41:44 -0400

    Updated CORS

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,14 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? process.env.MAIN_URL: 'http://localhost:5000'
-export const socket = io(URL);
+
+const BACKEND_URL = process.env.MAIN_URL;
+
+const URL = process.env.NODE_ENV === 'production' 
+    ? BACKEND_URL
+    : 'http://localhost:5000';
+
+export const socket = io(URL, {
+    transports: ['websocket'],
+    cors:{
+        origin: BACKEND_URL
+    }
+});
