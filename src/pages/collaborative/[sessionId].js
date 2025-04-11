722bc59df97bf8198671473f5a21d07aed06c2ae
Prakhar <rampalliprakhar@gmail.com>
2025-04-11 19:07:09 -0400

    Initial Fix: Dispatch Issue

--- src/pages/collaborative/[sessionId].js
+++ src/pages/collaborative/[sessionId].js
@@ -1,22 +1,22 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import { socket } from '@/socket'
 import Menu from '@/components/Menu'
 import Tools from '@/components/Tools'
 import Board from '@/components/Board'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setSessionId } from '@/slice/sessionSlice'
 
 export default function CollaborativeSession() {
   const router = useRouter();
   const { sessionId } = router.query;
   const initialized = useSelector(state => state.session.initialized);
+  const dispatch = useDispatch();
 //  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
     if (!sessionId) return;
-    dispatch(setSessionId(sessionId));
 
     const initSession = async () => {
       try {
@@ -31,8 +31,9 @@ export default function CollaborativeSession() {
           .from('whiteboard_sessions')
           .upsert(session, { onConflict: 'id' });
 
+        dispatch(setSessionId(sessionId));
         socket.emit('joinSession', sessionId);
-        setIsReady(true);
+        // setIsReady(true);
       } catch (error) {
         console.error('Session initialization failed:', error);
       }
